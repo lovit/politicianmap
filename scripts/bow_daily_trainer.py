130c2f528e869a69dff5af4eeb26eedc471efe59
lovit <soy.lovit@gmail.com>
2019-04-24 13:28:44 +0900

    Commit universal vocab scaning (for bow daily) script

--- scripts/bow_daily_trainer.py
+++ scripts/bow_daily_trainer.py
@@ -1,9 +1,16 @@
+import argparse
+import os
 from politicianmap.utils import check_dir
 from politicianmap.utils import News, DateDocsDecorator
-from politicianmap.utils import Tokenizer, Tagfilter, scan_vocabulary, create_bow
+from politicianmap.utils import Tokenizer, Tagfilter, scan_vocabulary
 
 
-def scan_universial_vocabulary(data_dir, index_dir, idxs, min_doc=15, debug=True):
+def write_list(path, items):
+    with open(path, 'w', encoding='utf-8') as f:
+        for item in items:
+            f.write('{}\n'.format(item))
+
+def make_merged_date_news(data_dir, index_dir, index, min_doc=15, debug=True):
     # variables for debug
     begin_date, end_date = '2018-01-01', '2018-01-10'
 
@@ -18,26 +25,28 @@ def scan_universial_vocabulary(data_dir, index_dir, idxs, min_doc=15, debug=True
 
     # merging news for all idxs
     news_sequence = []
-    for idx in idxs:
+    for idx in index:
         if debug:
             news = News('{}/{}/'.format(data_dir, idx), '{}/{}/'.format(index_dir, idx), begin_date, end_date)
         else:
             news = News('{}/{}/'.format(data_dir, idx), '{}/{}/'.format(index_dir, idx))
         news_sequence.append(news)
     merged_data_news = MergedNews([DateDocsDecorator(news, min_doc=min_doc) for news in news_sequence])
+    return merged_data_news
 
-    # set tokenizer and scan vocabulary
+def scaning_vocabulary(date_news, debug=True):
     tokenizer = Tokenizer(Tagfilter({'/R'}))
-    idx_to_vocab, vocab_to_idx = scan_vocabulary(merged_data_news, tokenizer, min_count=5 if debug else 20)
-    return vocab_to_idx
+    idx_to_vocab, vocab_to_idx = scan_vocabulary(date_news, tokenizer, min_count=5 if debug else 20)
+    return idx_to_vocab, vocab_to_idx
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default='/workspace/lovit/politicianmap/tokenized/')
     parser.add_argument('--index_dir', type=str, default='/workspace/data/politician_norm/')
-    parser.add_argument('--output_dir', type=str, default='/workspace/lovit/politicianmap/bow/')
+    parser.add_argument('--output_dir', type=str, default='/workspace/lovit/politicianmap/bow_daily/')
+    parser.add_argument('--min_doc_of_a_day', type=int, default=15)
     parser.add_argument('--index', type=int, nargs='*', default=None)
-    parser.add_argument('--max_index', type=int, default=20, default=None)
+    parser.add_argument('--max_index', type=int, default=20)
     parser.add_argument('--debug', dest='debug', action='store_true')
     parser.add_argument('--use_universal_vocab', dest='use_universal_vocab', action='store_true')
 
@@ -45,12 +54,21 @@ def main():
     data_dir = os.path.abspath(args.data_dir)
     index_dir = os.path.abspath(args.index_dir)
     output_dir = os.path.abspath(args.output_dir)
-    check_dir(output_dir)
 
+    min_doc = args.min_doc_of_a_day
     debug = args.debug
     index = args.index
     if index is None:
         index = [i for i in range(args.max_index)]
+
+    # scan universal vocabulary
+    merged_data_news = make_merged_date_news(data_dir, index_dir, index, min_doc, debug)
+    idx_to_vocab_univ, vocab_to_idx_univ = scaning_vocabulary(merged_data_news, debug)
+    path = '{}/universal_vocab.txt'.format(output_dir)
+    check_dir(path)
+    write_list(path, idx_to_vocab_univ)
+
+    # scan their own vocabulary and vectorize
 
 if __name__ == '__main__':
     main()
